8505fa9d87564d7179b020d6474334b131593e54
David Callu <callu.david@gmail.com>
2018-03-30 23:33:00 +0200

    feat(version): bump to 1.0.2o-1

--- id.cmake
+++ id.cmake
@@ -1,14 +1,14 @@
 set(package OpenSSL)
-set(version 1.0.2n)
+set(version 1.0.2o)
 
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-2
+    VERSION ${version}-1
     ABI 1.0.2
     )
 
 byd__package__download_info(${package}
-    URL "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2n.tar.gz"
-    URL_HASH SHA1=6d507bb849c8156f14c2b6f3e269a5e782ff6b82
+    URL "https://github.com/openssl/openssl/archive/OpenSSL_1_0_2o.tar.gz"
+    URL_HASH SHA1=ec7ad3650206f1045d48f2fe9ed4ad93a1228ec5
     )
