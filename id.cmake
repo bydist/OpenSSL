6b4c9e34a8a908fbb01bb765b97eca3241eee7e6
David Callu <callu.david@gmail.com>
2018-06-28 01:05:57 +0200

    fix(cmake): change BUYILD_TESTING to BUILD_TESTING

--- id.cmake
+++ id.cmake
@@ -4,7 +4,7 @@ set(version 1.0.2o)
 byd__package__info(${package}
     MAINTAINER_NAME "David Callu"
     MAINTAINER_EMAIL "callu.david@gmail.com"
-    VERSION ${version}-1
+    VERSION ${version}-2
     ABI 1.0.2
     )
 
